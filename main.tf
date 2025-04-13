8558eefb8be5fccab9408ffb0b3ab3fa97d38b12
chiranjeevi583 <gcpchiranjeevi@gmail.com>
2025-04-14 00:31:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,18 @@
 provider "google" {
-  project     = "gcp-dev-space"   # Updated project ID
-  region      = "us-central1"     # Change if needed
+  project     = "gcp-dev-space"
+  region      = "us-central1"
   credentials = file("terraform.json")
 }
 
 resource "google_compute_instance" "web_server" {
   name         = "web-server-instance"
-  machine_type = "f1-micro"        # Change if needed
+  machine_type = "f1-micro"
   zone         = "us-central1-a"
-  tags = ["http-server"]
+  tags         = ["http-server"]
+
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-11"  # Use your preferred OS
+      image = "debian-cloud/debian-11"
     }
   }
 
@@ -23,10 +24,23 @@ resource "google_compute_instance" "web_server" {
   metadata_startup_script = <<-EOT
     #!/bin/bash
     apt-get update
-    apt-get install -y apache2  # Change to nginx if needed
+    apt-get install -y apache2
     systemctl start apache2
     systemctl enable apache2
   EOT
+}
+
+resource "google_compute_firewall" "allow_http" {
+  name    = "allow-http"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["http-server"]
 }
 
 output "external_ip" {
